7d4693f4e83738f5a08f5bd48515df64d455b42c
Doug Winter <doug.winter@isotoma.com>
2017-01-10 15:32:38 +0000

    tests actually run

--- test.config.js
+++ test.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 module.exports = {
   resolve: {
     alias: {
-      'aws-cognito-sdk': path.resolve('/', '${__dirname}/node_modules/amazon-cognito-identity-js/dist/aws-cognito-sdk.js.bork'),
+      'aws-cognito-sdk': path.resolve(__dirname, 'node_modules/amazon-cognito-identity-js/dist/aws-cognito-sdk.js'),
     },
     extensions: ['', '.js', '.jsx'],
   }
