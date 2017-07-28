de2b62bcd8a5aba6d07d24fe010e72aea2f16ea1
Dave Bell <david.bell@isotoma.com>
2017-07-28 15:04:18 +0100

    removing examples moved to own repo react-cognito-example

--- examples/htdocs/vendor/fetch-example-dependencies.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-curl -o aws-cognito-sdk.min.js 'https://raw.githubusercontent.com/aws/amazon-cognito-identity-js/master/dist/aws-cognito-sdk.min.js' 
-curl -o amazon-cognito-identity.min.js 'https://raw.githubusercontent.com/aws/amazon-cognito-identity-js/master/dist/amazon-cognito-identity.min.js'
-curl -o jsbn.js 'http://www-cs-students.stanford.edu/~tjw/jsbn/jsbn.js'
-curl -o jsbn2.js 'http://www-cs-students.stanford.edu/~tjw/jsbn/jsbn2.js'
-curl -o sjcl.js 'https://raw.githubusercontent.com/bitwiseshiftleft/sjcl/master/sjcl.js'
